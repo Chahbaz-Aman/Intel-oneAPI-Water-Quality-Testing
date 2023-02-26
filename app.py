bdfbe15a8c7c0b7166c8aaeb303741a509f56fff
CHAHBAZ AMAN <chahbaza@gmail.com>
2023-02-26 18:38:16 +0530

    basic inference app
    added functionality to upload a file and get the drinkability test results

--- app.py
+++ app.py
@@ -1,39 +1,85 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import warnings
+import xgboost as xgb
 
-st.title('Uber pickups in NYC')
+warnings.filterwarnings('ignore')
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+st.title('Is your water drinkable?')
+
+TF_PATH = 'models/Intel_oneDNN_TF_NeuralNet'
+XGB_PATH = 'models/Intel_oneDAL_XGBoost'
+DTYPES = {'Index': 'int32',
+          'pH': 'float32',
+          'Iron': 'float32',
+          'Nitrate': 'float32',
+          'Chloride': 'float32',
+          'Lead': 'float64',
+          'Zinc': 'float32',
+          'Color': 'object',
+          'Turbidity': 'float32',
+          'Fluoride': 'float32',
+          'Copper': 'float32',
+          'Odor': 'float32',
+          'Sulfate': 'float64',
+          'Conductivity': 'float64',
+          'Chlorine': 'float64',
+          'Manganese': 'float32',
+          'Total Dissolved Solids': 'float64',
+          'Source': 'object',
+          'Water Temperature': 'float64',
+          'Air Temperature': 'float64',
+          'Month': 'object',
+          'Day': 'float16',
+          'Time of Day': 'float16',
+          }
+MONTHS = {'January': 1,
+          'February': 2,
+          'March': 3,
+          'April': 4,
+          'May': 5,
+          'June': 6,
+          'July': 7,
+          'August': 8,
+          'September': 9,
+          'October': 10,
+          'November': 11,
+          'December': 12,
+          }
+
+uploaded_file = st.file_uploader("Upload test results in CSV format")
+model = xgb.XGBClassifier()
+model.load_model(f'{XGB_PATH}/model.json')
+X_test = None
 
 
 @st.cache_data
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    def lowercase(x): return str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+def load_test_results():
+    df = pd.read_csv(uploaded_file, dtype=DTYPES)
+    if set(df.columns) != set(DTYPES.keys()):
+        st.write(df.columns)
+        st.write([col for col in df.columns if col not in list(DTYPES.keys())])
+        st.write("Uh-oh! Wrong file or incomplete results.")
+        return None
+
+    df = df[list(DTYPES.keys())]  # rearrange columns if necessary
+
+    if df.isna().sum().max() != 0:
+        st.write("File has missing values!")
+        return None
+
+    df['Month'] = df['Month'].apply(lambda m: MONTHS[m])
+    df.set_index('Index', inplace=True)
+    df = pd.get_dummies(df)
+
+    return df.values
 
 
-data_load_state = st.text('Loading data...')
-data = load_data(10000)
-data_load_state.text("Done! (using st.cache_data)")
+if uploaded_file is not None:
+    X_test = load_test_results()
 
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
-
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(
-    data[DATE_COLUMN].dt.hour, bins=24, range=(0, 24))[0]
-st.bar_chart(hist_values)
-
-# Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
+if X_test is not None:
+    model_output = model.predict(X_test)
+    st.write(model_output)
